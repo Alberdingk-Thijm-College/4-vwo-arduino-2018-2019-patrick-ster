e8c1d7ab6cc5cbc4a492b9ef81acc26b03c842de
kris <kriskras115@gmail.com>
2019-01-11 12:34:20 +0100

    simpele stoplicht
    les 2
    knipper, knipper, knipper

--- Basis_Opdracht/Basis_Opdracht.ino
+++ Basis_Opdracht/Basis_Opdracht.ino
@@ -1,9 +1,34 @@
-void setup() 
-{
-  // put your setup code here, to run once:
+int linkerPin = 11; 
+int middenPin = 10;
+int rechterPin = 9;
+
+
+void setup() {
+  pinMode(linkerPin, OUTPUT);
+  pinMode(middenPin, OUTPUT);
+  pinMode(rechterPin, OUTPUT);
 }
 
-void loop() 
-{
-  // put your main code here, to run repeatedly:
+
+
+void loop() {
+  digitalWrite(linkerPin, HIGH);
+  digitalWrite(middenPin, LOW);
+  digitalWrite(rechterPin, LOW);
+
+ delay(10000);
+ digitalWrite(linkerPin, LOW);
+ digitalWrite(middenPin, HIGH);
+ digitalWrite(rechterPin, LOW);
+
+ delay(10000);
+ digitalWrite(linkerPin, LOW);
+ digitalWrite(middenPin, LOW);
+ digitalWrite(rechterPin, HIGH);
+
+
+  
+
+  delay(10000);
+
 }
